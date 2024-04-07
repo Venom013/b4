05f11deed42acb3e9c4b226af7885d0ee8df4359
Edige <eddj21@gmail.com>
2024-04-07 16:37:31 +0600

    POST form

--- src/js/script.js
+++ src/js/script.js
@@ -33,8 +33,82 @@ $(document).ready(function(){
         })
       });
     };
-    toggleSlide('.catalog-item__link')
-    toggleSlide('.catalog-item__back')
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    //modal
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('show');
+    });
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+    // $('.button_mini').on('click', function() {
+    //   $('.overlay, #order').fadeIn('show');
+    // });
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('show');
+      })
+    });
+    
+    // Валидация форм
+    function validateForms(form){
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: {
+            required: "Пожалуйста, введите свое имя",
+            minlength: jQuery.validator.format("Введите {0} символова!")
+          },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправльно ввелен адрес почты"
+          }
+        }
+      });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#order form');
+    validateForms('#consultation form');
+
+    $('input[name=phone]').mask("+7 (999) 999 99 99")
+
+    /**Без перезагурзка страницы при отправке форм */
+    $('form').submit(function(e) {
+      e.preventDefault();
+      
+      if(!$(this).valid()){
+        return;
+      }
+
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+      });
+      
+      return false;
+    });
 });
 
 const slider = tns({
