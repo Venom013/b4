96627cc9c5b37703bc1c3c6cad458f04cd1e40b8
Edige <eddj21@gmail.com>
2024-04-07 16:42:11 +0600

    POST

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -18,7 +18,7 @@ $mail->Password = 'znij qucu hcqg tpuc';                           // Наш п�
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('eddj21@gmail.com', 'Pulse');   // От кого письмо 
+$mail->setFrom('info@gmail.com', 'Pulse');   // От кого письмо 
 $mail->addAddress('eddj21@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
